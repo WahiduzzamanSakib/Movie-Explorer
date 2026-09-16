cded1092129f2f129f844cfed697286927b7857d
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-09-16 22:51:37 +0600

    Navbar Add

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+ <h1 className="text-4xl font-bold text-center mt-10">
+      Movie Explorer
+    </h1>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-950 text-white border-b border-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+
+          {/* Logo */}
+          <a href="/" className="text-2xl font-bold text-red-500">
+            🎬 Movie<span className="text-white">Explorer</span>
+          </a>
+
+          {/* Navigation */}
+          <div className="flex items-center gap-4 sm:gap-8">
+            <a
+              href="/"
+              className="text-gray-300 hover:text-white transition"
+            >
+              Home
+            </a>
+
+            <a
+              href="/movies"
+              className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded-lg font-medium transition"
+            >
+              Movies
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
